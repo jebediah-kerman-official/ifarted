f268c5e10972af407e6d771a529632ab47e5653a
jebediah-kerman-official <123623888+jebediah-kerman-official@users.noreply.github.com>
2023-12-27 14:56:54 +0100

    modified:   bot/telegram_bot.py 	modified:   translations.json

--- bot/telegram_bot.py
+++ bot/telegram_bot.py
@@ -1058,8 +1058,6 @@ class ChatGPTTelegramBot:
 
         application.add_handler(CommandHandler('reset', self.reset))
         application.add_handler(CommandHandler('help', self.help))
-        application.add_handler(CommandHandler('image', self.image))
-        application.add_handler(CommandHandler('tts', self.tts))
         application.add_handler(CommandHandler('start', self.help))
         application.add_handler(CommandHandler('stats', self.stats))
         application.add_handler(CommandHandler('resend', self.resend))
